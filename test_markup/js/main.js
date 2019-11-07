4b86537a0568f516483fc6f2e0808c493255845b
Oleksandr Savchuk <sashasavch@gmail.com>
2019-11-08 00:55:03 +0200

    Added footer; adjusted elements positioning using PerfectPixel

--- test_markup/js/main.js
+++ test_markup/js/main.js
@@ -1,5 +1,4 @@
 // open menu
-$('.nav').addClass('open');
 $('.bt-nav').click( function() {
     $(this).parent().toggleClass( function() {
         if ( $(this).hasClass('open') ) {
@@ -11,13 +10,9 @@ $('.bt-nav').click( function() {
     });
 });
 
-if ($(window).width() < 768) {
-    if ($('.nav > nav > ul > li:first-child').attr('href') !== '#nav-home') {
-        $('.nav > nav > ul').prepend('<li><a href="#nav-home"><span data-hover="home">home</span></a></li>');
-    }
-}
-
 $(window).resize(function () {
+    $('.nav.open.close').removeClass('close');
+
     if ($(window).width() < 768) {
         var mainTop = $('.section-main').height();
 
@@ -25,27 +20,33 @@ $(window).resize(function () {
             var currentScroll = $(window).scrollTop(); // get current position
 
             if (currentScroll >= mainTop) {           // apply position: fixed if you
-                $('.bt-nav').css({                      // scroll to that element or below it
-                    position: 'fixed',
-                    right: '13px',
-                    top: '31px',
-                    background: '#000'
-                }).closest('.nav').find('nav').css({
-                    position: 'fixed'
-                });
-            } else {                                   // apply position: static
-                $('.bt-nav').css({                      // if you scroll above it
-                    position: 'absolute',
-                    background: 'transparent'
-                }).closest('.nav').find('nav').css({
-                    position: 'absolute'
-                });
+                                                      // scroll to that element or below it
+                $('.bt-nav').addClass('nav-fixed').closest('.nav').find('nav').addClass('pos-fixed');
+
+                // $('.bt-nav').css({
+                //     position: 'fixed',
+                //     right: '13px',
+                //     top: '31px',
+                //     background: '#000'
+                // }).closest('.nav').find('nav').css({
+                //     position: 'fixed'
+                // });
+            } else {
+                $('.bt-nav').removeClass('nav-fixed').closest('.nav').find('nav').removeClass('pos-fixed');
+                                                        // apply position: static
+                                                        // if you scroll above it
+                // $('.bt-nav').css({
+                //     position: 'absolute',
+                //     background: 'transparent'
+                // }).closest('.nav').find('nav').css({
+                //     position: 'absolute'
+                // });
             }
         });
     }
 });
 
-$('a[href^="#"]').on('click', function(event) {
+$('.a-scroll').on('click', function(event) {
     var target = $(this.getAttribute('href'));
     if( target.length ) {
         event.preventDefault();
@@ -57,19 +58,6 @@ $('a[href^="#"]').on('click', function(event) {
         $('.nav.open.close').removeClass('close')
     }
 });
-
-// $('button.button--effect-2').on('click', function(event) {
-//     var target = $(this.getAttribute('href'));
-//     if( target.length ) {
-//         event.preventDefault();
-//         $('html, body').stop().animate({
-//             scrollTop: target.offset().top
-//         }, 1000);
-//     }
-//     if($('.nav.open.close')) {
-//         $('.nav.open.close').removeClass('close')
-//     }
-// });
 
 $('.owl-carousel').owlCarousel({
     loop:true,
@@ -119,6 +107,8 @@ $('.button-group').each( function( i, buttonGroup ) {
         $( this ).addClass('is-checked');
     });
 });
+
+$($('.filters-button-group button')[0]).trigger('click');
 
 (function() {
 
@@ -186,3 +176,40 @@ $('.button-group').each( function( i, buttonGroup ) {
     } );
 
 })();
+
+// map = new OpenLayers.Map("demoMap");
+// map.addLayer(new OpenLayers.Layer.OSM());
+// map.zoomToMaxExtent();
+
+// $('body').load(init());
+//
+// function init() {
+//     map = new OpenLayers.Map("basicMap");
+//     var mapnik         = new OpenLayers.Layer.OSM();
+//     var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+//     var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+//     var position       = new OpenLayers.LonLat(13.41,52.52).transform( fromProjection, toProjection);
+//     var zoom           = 15;
+//
+//     map.addLayer(mapnik);
+//     map.setCenter(position, zoom );
+// }
+// 50.7469, 25.3233
+
+var lat            = 50.7469;
+var lon            = 25.3233;
+var zoom           = 12;
+
+var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+var position       = new OpenLayers.LonLat(lon, lat).transform( fromProjection, toProjection);
+
+map = new OpenLayers.Map("Map");
+var mapnik         = new OpenLayers.Layer.OSM();
+map.addLayer(mapnik);
+
+var markers = new OpenLayers.Layer.Markers( "Markers" );
+map.addLayer(markers);
+markers.addMarker(new OpenLayers.Marker(position));
+
+map.setCenter(position, zoom);
